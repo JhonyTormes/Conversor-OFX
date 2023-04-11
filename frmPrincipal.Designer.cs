0e890bd4714f4c303c53e439eb731f82381716f1
Jhony Tormes <jhony_pw@hotmail.com>
2023-04-11 13:34:07 -0300

    Alterações no layout e correção para salvar o valor do TextBox

--- frmPrincipal.Designer.cs
+++ frmPrincipal.Designer.cs
@@ -29,119 +29,112 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.abrirToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.salvarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.converterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.txtConteudo = new System.Windows.Forms.TextBox();
-            this.menuStrip1.SuspendLayout();
+            this.txtXML = new System.Windows.Forms.TextBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnConverter = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.abrirToolStripMenuItem,
-            this.editarToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // abrirToolStripMenuItem
-            // 
-            this.abrirToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.abrirToolStripMenuItem1,
-            this.salvarComoToolStripMenuItem,
-            this.sairToolStripMenuItem});
-            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
-            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
-            this.abrirToolStripMenuItem.Text = "Arquivo";
-            // 
-            // abrirToolStripMenuItem1
-            // 
-            this.abrirToolStripMenuItem1.Name = "abrirToolStripMenuItem1";
-            this.abrirToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.abrirToolStripMenuItem1.Size = new System.Drawing.Size(181, 22);
-            this.abrirToolStripMenuItem1.Text = "Abrir";
-            this.abrirToolStripMenuItem1.Click += new System.EventHandler(this.abrirToolStripMenuItem1_Click);
-            // 
-            // salvarComoToolStripMenuItem
-            // 
-            this.salvarComoToolStripMenuItem.Name = "salvarComoToolStripMenuItem";
-            this.salvarComoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.salvarComoToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
-            this.salvarComoToolStripMenuItem.Text = "Salvar Como";
-            this.salvarComoToolStripMenuItem.Click += new System.EventHandler(this.salvarComoToolStripMenuItem_Click);
-            // 
-            // sairToolStripMenuItem
-            // 
-            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
-            this.sairToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-            this.sairToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
-            this.sairToolStripMenuItem.Text = "Sair";
-            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
-            // 
-            // editarToolStripMenuItem
-            // 
-            this.editarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.converterToolStripMenuItem});
-            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
-            this.editarToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
-            this.editarToolStripMenuItem.Text = "Editar";
-            // 
-            // converterToolStripMenuItem
-            // 
-            this.converterToolStripMenuItem.Name = "converterToolStripMenuItem";
-            this.converterToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
-            this.converterToolStripMenuItem.Text = "Converter";
-            this.converterToolStripMenuItem.Click += new System.EventHandler(this.converterToolStripMenuItem_Click);
             // 
             // txtConteudo
             // 
             this.txtConteudo.AcceptsReturn = true;
             this.txtConteudo.AcceptsTab = true;
-            this.txtConteudo.AllowDrop = true;
-            this.txtConteudo.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtConteudo.Location = new System.Drawing.Point(0, 24);
+            this.txtConteudo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtConteudo.Location = new System.Drawing.Point(12, 12);
             this.txtConteudo.Multiline = true;
             this.txtConteudo.Name = "txtConteudo";
-            this.txtConteudo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtConteudo.Size = new System.Drawing.Size(800, 426);
+            this.txtConteudo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtConteudo.Size = new System.Drawing.Size(450, 482);
             this.txtConteudo.TabIndex = 1;
+            this.txtConteudo.WordWrap = false;
+            // 
+            // txtXML
+            // 
+            this.txtXML.AcceptsReturn = true;
+            this.txtXML.AcceptsTab = true;
+            this.txtXML.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtXML.Location = new System.Drawing.Point(546, 12);
+            this.txtXML.Multiline = true;
+            this.txtXML.Name = "txtXML";
+            this.txtXML.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtXML.Size = new System.Drawing.Size(450, 482);
+            this.txtXML.TabIndex = 2;
+            this.txtXML.WordWrap = false;
+            this.txtXML.TextChanged += new System.EventHandler(this.txtXML_TextChanged);
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(12, 500);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(450, 23);
+            this.btnAbrir.TabIndex = 4;
+            this.btnAbrir.Text = "Abrir arquivo OFX";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(546, 502);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(450, 23);
+            this.btnSalvar.TabIndex = 5;
+            this.btnSalvar.Text = "Salvar arquivo XML";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(467, 289);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Location = new System.Drawing.Point(467, 258);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(75, 23);
+            this.btnConverter.TabIndex = 3;
+            this.btnConverter.Text = "Converter";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1008, 537);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.btnConverter);
+            this.Controls.Add(this.txtXML);
             this.Controls.Add(this.txtConteudo);
-            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "frmPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Conversor_OFX";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem salvarComoToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem converterToolStripMenuItem;
         private System.Windows.Forms.TextBox txtConteudo;
+        private System.Windows.Forms.TextBox txtXML;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnConverter;
     }
 }
 
--- frmPrincipal.cs
+++ frmPrincipal.cs
@@ -18,7 +18,13 @@ namespace Conversor_OFX
             InitializeComponent();
         }
 
-         private void abrirToolStripMenuItem1_Click(object sender, EventArgs e)
+
+        private void txtXML_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+
+        private void btnAbrir_Click(object sender, EventArgs e)
         {
             //Abre o arquivo e executa a leitura, retornando o seu conteúdo para a propriedade Caminho, para que
             //as informações possam ser acessadas pelos outros componentes.
@@ -26,16 +32,16 @@ namespace Conversor_OFX
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Filter = "Arquivo OFX| *.ofx";
             ofd.ShowDialog();
-            if (string.IsNullOrEmpty(ofd.FileName) == false)
+            if (!string.IsNullOrEmpty(ofd.FileName))
             {
                 try
                 {
                     using (StreamReader leitor = new StreamReader(ofd.FileName, Encoding.GetEncoding(CultureInfo.GetCultureInfo("pt-BR").TextInfo.ANSICodePage)))
                     {
                         txtConteudo.Text = leitor.ReadToEnd();
-                        Caminho= ofd;
+                        Caminho = ofd;
                     }
-        
+
                 }
                 catch (Exception ex)
                 {
@@ -43,51 +49,11 @@ namespace Conversor_OFX
                 }
             }
 
+                
+            
         }
 
-        private void salvarComoToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //Salva o arquivo permitindo salvar apenas caso o arquivo esteja com o padrão de XML da conversão utilizada.
-
-            if (txtConteudo.Text.StartsWith("<?xml"))
-            {
-                try
-                {
-                    string nomeArquivoSemExtensao = Path.GetFileNameWithoutExtension(Caminho.SafeFileName);
-                    SaveFileDialog sfd = new SaveFileDialog();
-                    sfd.Filter = "Arquivo XML | *.xml";
-                    sfd.FileName = nomeArquivoSemExtensao;
-                    sfd.ShowDialog();
-
-                    if (string.IsNullOrEmpty(sfd.FileName) == false)
-                    {
-                        using (StreamWriter writer = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
-                        {
-                                writer.Write(txtConteudo.Text);
-                                writer.Flush();
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-
-                    MessageBox.Show($"Não foi possível salvar o arquivo. Erro: {ex.Message}", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-            else
-            {
-                MessageBox.Show("Não foi possível salvar, o arquivo não está no formato de XML da conversão", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-        }
-
-        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-       
-
-        private void converterToolStripMenuItem_Click(object sender, EventArgs e)
+        private void btnConverter_Click(object sender, EventArgs e)
         {
             //Converte o conteúdo do arquivo aberto para o padrão XML, retirando os espaços caso o arquivo já esteja
             // identado, retirando os cabeçallhos do OFX e fechando todas as tags que não tenham fechamento.
@@ -96,7 +62,8 @@ namespace Conversor_OFX
             {
                 try
                 {
-                    String[] linhas = File.ReadAllLines(Caminho.FileName.TrimStart());
+                    String[] linhas = txtConteudo.Lines;
+
                     XmlDocument arquivoXml = new XmlDocument();
                     var escritor = new StringBuilder();
 
@@ -119,20 +86,75 @@ namespace Conversor_OFX
                         }
                     }
                     arquivoXml.LoadXml(escritor.ToString());
-                    txtConteudo.Text = TratarXml.Identador(arquivoXml);
+                    txtXML.Text = TratarXml.Identador(arquivoXml);
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
 
-                    MessageBox.Show($"Não foi possível converter o arquivo. Erro: {ex.Message}", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show($"Não foi possível converter o arquivo.\nCertifique-se de abrir um arquivo válido antes de converter"
+                                    , "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
 
 
             }
             else
             {
-                MessageBox.Show($"Arquivo vazio.\nNão foi possível converter.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show($"Não foi possível converter o arquivo.\nNão há nenhum conteúdo para converter"
+                 , "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            //Salva o arquivo permitindo salvar apenas caso o arquivo esteja com o padrão de XML da conversão utilizada.
+
+            if (txtXML.Text.StartsWith("<?xml"))
+            {
+                try
+                {
+                    string nomeArquivoSemExtensao = Path.GetFileNameWithoutExtension(Caminho.SafeFileName);
+                    SaveFileDialog sfd = new SaveFileDialog();
+                    sfd.Filter = "Arquivo XML | *.xml";
+                    sfd.FileName = nomeArquivoSemExtensao;
+                    sfd.ShowDialog();
+
+                    if (!string.IsNullOrEmpty(sfd.FileName))
+                    {
+                        using (StreamWriter writer = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+                        {
+                            writer.Write(txtXML.Text);
+                            writer.Flush();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show($"Não foi possível salvar o arquivo. Erro: {ex.Message}", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Não foi possível salvar, o arquivo não está no formato de XML da conversão", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtConteudo.Text) || !string.IsNullOrEmpty(txtXML.Text))
+            {
+                DialogResult dialogResult = MessageBox.Show("A conversão atual será perdida\nTem certeza que deseja limpar os textos?", "Atenção!", MessageBoxButtons.OKCancel,MessageBoxIcon.Warning) ;
+                if (dialogResult == DialogResult.OK)
+                {
+                        txtConteudo.Clear();
+                        txtXML.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Não há nenhum conteúdo para limpar","Atenção!",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+            
         }
     }
 }
